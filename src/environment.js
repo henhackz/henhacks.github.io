eb57a029209eb6de1070bd563b99153f991dc317
ydnamjs <ydnamjs@udel.edu>
2024-03-02 14:24:06 -0500

    Added positional audio sound example that plays when scene opens

--- src/environment.js
+++ src/environment.js
@@ -1,12 +1,22 @@
 import * as THREE from 'three';
 
+const SOUNDS_PATH = "../assets/sounds/"
+
+let BADGLOBALVARIABLE
+
 export function renderEnvironment(renderer, camera, scene) {
     
     camera.position.z = 5;
 
+    const listener = new THREE.AudioListener();
+    camera.add( listener );
+
+    addSound(listener)
+
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     const cube = new THREE.Mesh(geometry, material);
+    addSound(listener, cube);
     scene.add(cube);
 
     function animate() {
@@ -18,4 +28,19 @@ export function renderEnvironment(renderer, camera, scene) {
         renderer.render(scene, camera);
     }
     animate();
+}
+
+function addSound(listener, mesh) {
+    const sound = new THREE.PositionalAudio( listener );
+
+    const audioLoader = new THREE.AudioLoader();
+    audioLoader.load( '/assests/sounds/piano-test.mp3', function( buffer ) {
+        sound.setBuffer( buffer );
+        sound.setRefDistance( 20 );
+        sound.play();
+    });
+
+    console.log(mesh)
+
+    //mesh.add(sound)
 }
