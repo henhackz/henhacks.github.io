d2cf7f2fea2be7be47137da59886683cd2c19027
ydnamjs <ydnamjs@udel.edu>
2024-03-02 17:44:54 -0500

    Added custom delays to positional sounds

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 
-const SOUNDS_PATH = "../assets/sounds/"
+function delay(ms) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
 
 export function renderEnvironment(renderer, camera, scene) {
     
@@ -9,12 +12,9 @@ export function renderEnvironment(renderer, camera, scene) {
     const listener = new THREE.AudioListener();
     camera.add( listener );
 
-    const sound1 = makeSound(listener, '/assests/sounds/dream/ambient-dream-16671.mp3');
-
-    const sound2 = makeSound(listener, '/assests/sounds/dream/dream-sound-effect-downscale-7134.mp3');
+    const [sound1, timer1] = makeSound(listener, '/assests/sounds/dream/dream-sound-effect-downscale-7134.mp3', 0.0);
 
     const cube1 = makeCube(0, 0, 0, scene, sound1);
-    const cube2 = makeCube(2, 0, 0, scene, sound2);
 
     function animate() {
         requestAnimationFrame(animate);
@@ -46,15 +46,43 @@ function makeCube(x, y, z, scene, sound) {
     return cube
 }
 
-function makeSound(listener, soundPath) {
-    const sound = new THREE.PositionalAudio( listener );
+function makeSound(listener, soundPath, replayDelay) {
+    
+    let sound = new THREE.PositionalAudio( listener );
+    
+    if(replayDelay > 0) {
+    
+        let timer = undefined;
+        const audioLoader = new THREE.AudioLoader();
 
-    const audioLoader = new THREE.AudioLoader();
-    audioLoader.load(soundPath, function( buffer ) {
-        sound.setBuffer( buffer );
-        sound.play();
-        sound.setLoop(true);
-    });
+        audioLoader.load(soundPath, async function( buffer ) {
+            sound.setBuffer( buffer );
+            sound.setLoop(true)
 
-    return sound;
+            sound.play()
+
+            timer = setInterval(async () => {
+                sound.play()
+                await delay(buffer.duration * 1000)
+                sound.pause()
+            }, (buffer.duration + replayDelay) * 1000)
+
+            await delay(buffer.duration * 1000)
+            sound.pause()
+        })
+    
+        return [sound, timer]
+
+    } 
+    
+    else {
+        const audioLoader = new THREE.AudioLoader();
+        audioLoader.load(soundPath, function( buffer ) {
+            sound.setBuffer( buffer );
+            sound.play();
+            sound.setLoop(true);
+        });
+
+        return [sound, undefined]
+    }
 }
