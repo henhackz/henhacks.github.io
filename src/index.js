3f3b4c403e224a42faaa6e952054d25bee0b8695
bladetyr <bladetyr@udel.edu>
2024-03-02 17:40:55 -0500

    basic posting

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/', (req, res) => {
+    res.send('Thanks for your POST')
+    console.log('omg hiiii');
+})
