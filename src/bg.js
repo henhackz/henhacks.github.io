479d9c1463e2be092c938268f85d2093f2fb6820
Blair <blairf@udel.edu>
2024-03-03 04:00:36 -0500

    oopsie

--- src/bg.js
+++ src/bg.js
@@ -7,6 +7,7 @@ document.getElementById("1").addEventListener("click", function() {
     document.body.style.background = "radial-gradient(circle, rgba(149,127,173,1) 0%, rgba(101,59,147,1) 100%)"
     removeScenes();
     renderDreamEnvironment();
+});
 document.getElementById("2").addEventListener("click", function() {
     console.log(2);
     document.body.style.background = "radial-gradient(circle, rgba(113,160,169,1) 0%, rgba(39,72,148,1) 100%)"
