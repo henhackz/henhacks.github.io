7185a6493d3c6e100765506e09d591bd6dcb4897
ydnamjs <ydnamjs@udel.edu>
2024-03-03 02:47:32 -0500

    Added functionality to button to summon the dream environment note that it does not remove it

--- src/bg.js
+++ src/bg.js
@@ -1,12 +1,14 @@
+import {renderDreamEnvironment, removeDreamEnvironment} from './enviroments/dream.js'
+
 document.getElementById("1").addEventListener("click", function() {
     console.log(1);
     document.body.style.background = "radial-gradient(circle, rgba(149,127,173,1) 0%, rgba(101,59,147,1) 100%)"
-    //play ourple
+    renderDreamEnvironment();
 });
 document.getElementById("2").addEventListener("click", function() {
     console.log(2);
     document.body.style.background = "radial-gradient(circle, rgba(113,160,169,1) 0%, rgba(39,72,148,1) 100%)"
-    //play ocean
+    removeDreamEnvironment()
 });
 document.getElementById("3").addEventListener("click", function() {
     console.log(3);
--- src/enviroments/dream.js
+++ src/enviroments/dream.js
@@ -1,39 +1,50 @@
 import * as THREE from 'three';
 import {renderEnvironment} from "../environment.js"
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const renderingData = {renderer: renderer, camera: camera, scene: scene}
-
-const dreamEnvironmentData = [
-    {
-        soundPath: '../../assests/sounds/dream/dream-sound-effect-downscale-7134.mp3', 
-        replayDelay: 1,
-        x: -8,
-        y: 0,
-        z: 5
-    },
-    {
-        soundPath: '../../assests/sounds/dream/ambient-dream-16671.mp3', 
-        replayDelay: 1,
-        x: 0,
-        y: -5,
-        z: 0
-    },
-    {
-        soundPath: '../../assests/sounds/dream/various-glassy-stone-windchime-sounds-48417.mp3', 
-        replayDelay: 1,
-        x: 0,
-        y: 0,
-        z: 5
-    }
-]
+let renderer;
 
 export function renderDreamEnvironment() {
-    renderEnvironment(renderingData, dreamEnvironmentData)
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    
+    const renderingData = {renderer: renderer, camera: camera, scene: scene}
+    
+    const dreamEnvironmentData = [
+        {
+            soundPath: '../../assests/sounds/dream/dream-sound-effect-downscale-7134.mp3', 
+            replayDelay: 1,
+            x: -8,
+            y: 0,
+            z: 0
+        },
+        {
+            soundPath: '../../assests/sounds/dream/ambient-dream-16671.mp3', 
+            replayDelay: 1,
+            x: 3,
+            y: 0,
+            z: 0
+        },
+        {
+            soundPath: '../../assests/sounds/dream/various-glassy-stone-windchime-sounds-48417.mp3', 
+            replayDelay: 1,
+            x: -5,
+            y: 1,
+            z: 0
+        }
+    ]
+
+    renderEnvironment(renderingData, dreamEnvironmentData);
+
+    document.body.appendChild( renderer.domElement );
+}
+
+export function removeDreamEnvironment() {
+    if(renderer !== undefined) {
+        console.log("removing dream")
+        document.body.removeChild( renderer.domElement );
+        renderer = undefined;
+    }
 }
